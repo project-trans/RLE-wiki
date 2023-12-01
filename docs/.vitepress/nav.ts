7a243ce0494ce467669fdd171b4b4cafe4a4ebcc
undefined <i@undefined.moe>
2023-12-01 08:15:58 +0000

    markdown and sidebar enhancement (#220)

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,20 +1,26 @@
-// import { navbar } from "vuepress-theme-hope";
 import { DefaultTheme } from 'vitepress'
 
-export const configNav: DefaultTheme.Config['nav'] = [
-  { text: "大学指南",
-    link: "/campus/readme.md"
+const nav = [
+  {
+    text: "大学指南",
+    link: "/campus/",
   },
   {
     text: "Fashion",
-    link: "/fashion/readme.md"
+    link: "/fashion/",
   },
   {
     text: "贡献指南",
-    items: [
-      { text: "校园版块投稿指南", link: "/contributor-guide/campus.md" },
-      { text: "其他投稿指南", link: "/contributor-guide/other.md" },
-      { text: "校园版块贡献模板", link: "/contributor-guide/CampusTemplate.md" },
-    ],
+    items: [{
+      text: "校园版块投稿指南",
+      link: "/contributor-guide/campus.md",
+    }, {
+      text: "其他投稿指南",
+      link: "/contributor-guide/other.md",
+    }, {
+      text: "校园版块贡献模板",
+      link: "/contributor-guide/CampusTemplate.md",
+    }],
   },
-];
+] satisfies DefaultTheme.Config['nav'];
+export default nav;
