5709f3af90cbd403a3810045c13d55d4937efa9c
Rizumu Ayaka <rizumu@ayaka.moe>
2023-12-19 21:32:39 +0800

    feat(next): sidebarIs sorted by title

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,28 +1,53 @@
 import { type DefaultTheme } from 'vitepress'
-import { generateSidebar } from 'vitepress-sidebar'
+import { SidebarItem, generateSidebar } from 'vitepress-sidebar'
 
-export const sidebar =  generateSidebar([
-  // 大学指南
-  {
-    documentRootPath: '/docs',
-    scanStartPath: 'campus',
-    resolvePath: '/campus/',
-    useTitleFromFrontmatter: true,
-  },
-  // 贡献指南
-  {
-    documentRootPath: '/docs',
-    scanStartPath: 'contributor-guide',
-    resolvePath: '/contributor-guide/',
-    useTitleFromFrontmatter: true,
-  },
-  // Fashion
-  {
-    documentRootPath: '/docs',
-    scanStartPath: 'fashion',
-    resolvePath: '/fashion/',
-    useTitleFromFrontmatter: true,
+export const sidebar = generate()
+
+function generate() {
+  let sidebar = generateSidebar([
+    // 大学指南
+    {
+      documentRootPath: '/docs',
+      scanStartPath: 'campus',
+      resolvePath: '/campus/',
+      useTitleFromFrontmatter: true,
+    },
+    // 贡献指南
+    {
+      documentRootPath: '/docs',
+      scanStartPath: 'contributor-guide',
+      resolvePath: '/contributor-guide/',
+      useTitleFromFrontmatter: true,
+    },
+    // Fashion
+    {
+      documentRootPath: '/docs',
+      scanStartPath: 'fashion',
+      resolvePath: '/fashion/',
+      useTitleFromFrontmatter: true,
+    }
+
+    // 这个 `as` 源于 vitepress-sidebar 的类型定义与实际情况的差异，目前不影响使用，后续 vitepress-sidebar 修复后可以移除。
+  ]) as DefaultTheme.Config['sidebar']
+
+  for (const key in sidebar) {
+    sidebar[key].items.sort(sidebarTitleSorter)
   }
+  return sidebar
+}
 
-  // 这个 `as` 源于 vitepress-sidebar 的类型定义与实际情况的差异，目前不影响使用，后续 vitepress-sidebar 修复后可以移除。
-]) as DefaultTheme.Config['sidebar']
+function sidebarTitleSorter(
+  infoA: SidebarItem,
+  infoB: SidebarItem
+): number {
+  const textA = infoA.text
+  const textB = infoB.text
+  if (textA === undefined || textB === undefined) {
+    return 0
+  }
+  const infoANfc = textA.normalize('NFC');
+  const infoBNfc = textB.normalize('NFC');
+  return infoANfc.localeCompare(infoBNfc, 'zh', {
+      numeric: true,
+  })
+}
