fbe61c37fa99149eb904e04899a9fb722c2cbe33
Neko Ayaka <neko@ayaka.moe>
2023-12-21 23:41:33 +0800

    feat: enabled local search that natively supported by VitePress
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitepress'
 import nav from './nav'
-import mdPangu from "markdown-it-pangu"
+import mdPangu from 'markdown-it-pangu'
 import katex from 'markdown-it-katex'
 import footnote from 'markdown-it-footnote'
 import { sidebar } from './sidebar'
@@ -9,8 +9,8 @@ import { rootDir, githubRepoLink } from './meta'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   lang: 'zh-CN',
-  title: "RLE.wiki",
-  description: "一份 RLE 指北",
+  title: 'RLE.wiki',
+  description: '一份 RLE 指北',
   markdown: {
     config(md) {
       md.use(mdPangu);
@@ -24,16 +24,13 @@ export default defineConfig({
     siteTitle: 'RLE.wiki',
     nav,
     sidebar,
-
     socialLinks: [
       { icon: 'github', link: githubRepoLink }
     ],
-
     editLink: {
       pattern: `${githubRepoLink}/edit/main/docs/:path`,
       text: '在 GitHub 上编辑此页面', // label localization
     },
-
     // label localization
     outline: { label: '本页大纲', level: 'deep' },
     lastUpdated: { text: '最后更新' },
@@ -44,5 +41,28 @@ export default defineConfig({
       prev: '上一页',
       next: '下一页',
     },
-  }
-})
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
