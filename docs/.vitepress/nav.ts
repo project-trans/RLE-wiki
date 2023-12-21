66ab64bcae6c6a514894064229eed6d040c66a94
Neko Ayaka <neko@ayaka.moe>
2023-12-21 23:43:18 +0800

    feat: enabled local search that natively supported by VitePress (#236)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -4,25 +4,29 @@ type NavConfig = DefaultTheme.Config['nav']
 
 const nav: NavConfig = [
   {
-    text: "大学指南",
-    link: "/campus/",
+    text: '大学指南',
+    link: '/campus/',
   },
   {
-    text: "Fashion",
-    link: "/fashion/",
+    text: 'Fashion',
+    link: '/fashion/',
   },
   {
-    text: "贡献指南",
-    items: [{
-      text: "校园版块投稿指南",
-      link: "/contributor-guide/campus.md",
-    }, {
-      text: "其他投稿指南",
-      link: "/contributor-guide/other.md",
-    }, {
-      text: "校园版块贡献模板",
-      link: "/contributor-guide/CampusTemplate.md",
-    }],
+    text: '贡献指南',
+    items: [
+      {
+        text: '校园版块投稿指南',
+        link: '/contributor-guide/campus.md',
+      },
+      {
+        text: '其他投稿指南',
+        link: '/contributor-guide/other.md',
+      },
+      {
+        text: '校园版块贡献模板',
+        link: '/contributor-guide/CampusTemplate.md',
+      },
+    ],
   },
 ];
 
