a5f5f3e354433abb0a907de5c8a879d345ac8d98
Rizumu Ayaka <rizumu@ayaka.moe>
2023-12-22 18:46:29 +0800

    feat(next): soft appearance toggle animation

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    AppearanceToggle: typeof import('./theme/components/AppearanceToggle.vue')['default']
     Changelog: typeof import('./theme/components/Changelog.vue')['default']
     HomeContent: typeof import('./theme/components/HomeContent.vue')['default']
     PageInfo: typeof import('./theme/components/PageInfo.vue')['default']
