334d942de572b1a5dca0d3d15f31fe1e7accb6bc
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-02-06 20:02:55 +0800

    🎨 Add SuggestionBox

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,20 @@
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
-import { defineConfig } from "unocss";
+import presetSBox from 'pjts-suggestion-box/dist/preset';
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss';
 
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-flex justify-center gap-2 text-white leading-30px children:mya !no-underline cursor-pointer disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
   ],
+  content: {
+    pipeline: {
+      include: [
+        // Default match pattern of UnoCSS
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // Match suggestion-box
+        /.*pjts-suggestion-box.*\.js/,
+      ],
+    },
+  },
   presets: [
     presetUno({
       dark: 'class',
@@ -20,5 +30,6 @@ export default defineConfig({
       },
       warn: true,
     }),
+    presetSBox(),
   ],
 })
