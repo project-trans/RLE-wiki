b06ff1a675fe11509316de9d29e902ba785e43d1
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-02-06 20:02:55 +0800

    🎨 Add SuggestionBox

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppearanceToggle: typeof import('./theme/components/AppearanceToggle.vue')['default']
+    AppSBox: typeof import('./theme/components/AppSBox.vue')['default']
     Changelog: typeof import('./theme/components/Changelog.vue')['default']
     HomeContent: typeof import('./theme/components/HomeContent.vue')['default']
     PageInfo: typeof import('./theme/components/PageInfo.vue')['default']
---
+++ docs/.vitepress/theme/components/AppSBox.vue
@@ -0,0 +1,6 @@
+<template>
+  <SuggestionBox placeholder="写点什么" />
+</template>
+<script setup lang="ts">
+import SuggestionBox from 'pjts-suggestion-box';
+</script>
