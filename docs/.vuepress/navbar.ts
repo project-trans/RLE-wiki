366830b893347461cecf014027c8cfd4dd96d087
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-02-07 16:09:22 +0800

    📝 Add personal safety category and sexual-assault article

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,18 +1,24 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  { text: "大学指南",
-    icon: "creative",
-    link: "/campus/" 
-  },
+  { text: "大学指南", icon: "creative", link: "/campus/" },
   {
     text: "Fashion",
     icon: "creative",
-    link: "/fashion"
+    link: "/fashion",
+  },
+  {
+    text: "防护",
+    icon: "creative",
+    link: "/personal-safety",
   },
   {
     text: "贡献指南",
     icon: "info",
-    children: ["/contributor-guide/campus.md","/contributor-guide/other.md", "/contributor-guide/CampusTemplate.md"],
+    children: [
+      "/contributor-guide/campus.md",
+      "/contributor-guide/other.md",
+      "/contributor-guide/CampusTemplate.md",
+    ],
   },
 ]);
