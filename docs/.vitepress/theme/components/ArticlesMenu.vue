5317732940c60eaa125307c1611a00c1c55a43c5
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-03-06 22:37:38 +0800

    🔥 Remove unnecessary slot and defineProps()

--- docs/.vitepress/theme/components/ArticlesMenu.vue
+++ docs/.vitepress/theme/components/ArticlesMenu.vue
@@ -2,11 +2,9 @@
 import { useRoute } from "vitepress";
 import { data } from "./articlesmenu.data";
 const route = useRoute();
-
-defineProps<{ title?: string }>();
 </script>
+
 <template>
-  <slot />
   <ul>
     <li v-for="article in data.menu[route.path].items" :key="article.link">
       <a :href="article.link">{{ article.text }}</a>
