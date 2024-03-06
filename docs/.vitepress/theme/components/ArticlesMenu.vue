c8a887f500a269a96cfae844c068007733a39578
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-03-06 21:50:50 +0800

    ✨ Add articles menu

---
+++ docs/.vitepress/theme/components/ArticlesMenu.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useRoute } from "vitepress";
+import { data } from "./articlesmenu.data";
+const route = useRoute();
+
+defineProps<{ title?: string }>();
+</script>
+<template>
+  <slot />
+  <ul>
+    <li v-for="article in data.menu[route.path].items" :key="article.link">
+      <a :href="article.link">{{ article.text }}</a>
+    </li>
+  </ul>
+</template>
