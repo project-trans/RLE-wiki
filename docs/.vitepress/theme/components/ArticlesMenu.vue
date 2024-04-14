6361395286b7065283159853bf702da9a703e604
北雁云依 <14120445+BeiyanYunyi@users.noreply.github.com>
2024-04-14 11:54:41 +0800

    fix(ArticleMenu): :bug: wrong link in artiecle menu (#309)

--- docs/.vitepress/theme/components/ArticlesMenu.vue
+++ docs/.vitepress/theme/components/ArticlesMenu.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-import { useRoute } from "vitepress";
-import { data } from "./articlesmenu.data";
-const route = useRoute();
+import { useRoute } from 'vitepress'
+import { computed } from 'vue'
+import { data } from './articlesmenu.data'
+
+const route = useRoute()
+const articles = computed(() =>
+  data.menu[route.path].items.map(item => ({
+    ...item,
+    link: item.link.replace('.md', ''),
+  })),
+)
 </script>
 
 <template>
   <ul>
-    <li v-for="article in data.menu[route.path].items" :key="article.link">
+    <li v-for="article in articles" :key="article.link">
       <a :href="article.link">{{ article.text }}</a>
     </li>
   </ul>
