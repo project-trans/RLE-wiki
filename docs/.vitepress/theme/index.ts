e451db81fa7030e21882736ced2c6292453a3639
Neko Ayaka <neko@ayaka.moe>
2024-04-15 12:15:21 +0800

    docs: migrated to nolebase 2.0.0-rc3
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,18 +8,18 @@ import {
   NolebaseEnhancedReadabilitiesPlugin,
   NolebaseEnhancedReadabilitiesMenu,
   NolebaseEnhancedReadabilitiesScreenMenu,
-} from '@nolebase/vitepress-plugin-enhanced-readabilities'
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
 
 import {
   NolebaseHighlightTargetedHeading,
-} from '@nolebase/vitepress-plugin-highlight-targeted-heading'
+} from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
 
 import {
   NolebaseGitChangelogPlugin
 } from '@nolebase/vitepress-plugin-git-changelog/client'
 
-import '@nolebase/vitepress-plugin-enhanced-readabilities/dist/style.css'
-import '@nolebase/vitepress-plugin-highlight-targeted-heading/dist/style.css'
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
+import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 
 import 'uno.css'
