a06d14f3df95d8d189b4087d9335fb430284778a
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-04-20 09:46:56 +0800

    refactor(style): :art: Move changelog and suggestion box to footer

---
+++ docs/.vitepress/theme/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { NolebaseGitChangelog } from '@nolebase/vitepress-plugin-git-changelog/client'
+</script>
+
+<template>
+  <div class="vp-doc">
+    <h2>意见反馈</h2>
+    <AppSBox />
+    <h2>文件历史</h2>
+    <NolebaseGitChangelog />
+  </div>
+</template>
