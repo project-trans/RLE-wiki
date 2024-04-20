02288ce3f73fa16dfe368ea278a8928cc1ab58b6
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-04-20 10:43:50 +0800

    fix(AppFooter): :bug: Show SuggestionBox and Changelog in DocsAsideOutline

--- docs/.vitepress/theme/components/AppFooter.vue
+++ docs/.vitepress/theme/components/AppFooter.vue
@@ -4,9 +4,13 @@ import { NolebaseGitChangelog } from '@nolebase/vitepress-plugin-git-changelog/c
 
 <template>
   <div class="vp-doc">
-    <h2>意见反馈</h2>
+    <h2 id="意见反馈">
+      意见反馈
+    </h2>
     <AppSBox />
-    <h2>文件历史</h2>
+    <h2 id="文件历史">
+      文件历史
+    </h2>
     <NolebaseGitChangelog />
   </div>
 </template>
