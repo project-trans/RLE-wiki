6fb85d3256072ba0e0d8140169e54e36a5f59b53
Anna Beiyan <131662099+AnnaBeiyan@users.noreply.github.com>
2024-06-10 13:57:59 +0800

    refactor(config): :recycle: Refactor theme\index.ts

--- packages/vitepress-theme-project-trans/src/theme.ts
+++ packages/vitepress-theme-project-trans/src/theme.ts
@@ -3,6 +3,10 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
+import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
+import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css'
+
 import {
   NolebaseEnhancedReadabilitiesMenu,
   NolebaseEnhancedReadabilitiesPlugin,
