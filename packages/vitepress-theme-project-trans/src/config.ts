5d8325e73be3a95d3aedb3bc98aa538cd6d36e9c
LeeGuanLi <lee@mtftm.com>
2024-09-08 18:54:46 +0800

    Update search modal footer in config.ts

--- packages/vitepress-theme-project-trans/src/config.ts
+++ packages/vitepress-theme-project-trans/src/config.ts
@@ -102,9 +102,11 @@ function genConfig() {
                 modal: {
                   noResultsText: '无法找到相关结果',
                   resetButtonTitle: '清除查询条件',
+                  displayDetails: '显示详细列表',
                   footer: {
                     selectText: '选择',
                     navigateText: '切换',
+                    closeText: '关闭',
                   },
                 },
               },
