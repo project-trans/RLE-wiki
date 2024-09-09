8ca31977479d48022c91fac712037c90e1ec7cd8
LeeGuanLi <lee@mtftm.com>
2024-09-09 21:14:48 +0800

    refactor: 更新AppFooter.vue组件

--- packages/vitepress-theme-project-trans/src/components/AppFooter.vue
+++ packages/vitepress-theme-project-trans/src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch, onMounted } from 'vue';
 import { NolebaseGitChangelog } from '@nolebase/vitepress-plugin-git-changelog/client';
 import { useRoute } from 'vitepress';
 
@@ -8,13 +8,23 @@ const route = useRoute();
 // 定义一个 ref 来存储动态 key
 const componentKey = ref(0);
 const frontmatter = ref({});
+const isFrontmatterLoaded = ref(false);
+
+// 更新 key 和 frontmatter 的函数
+const updateKeyAndFrontmatter = () => {
+  componentKey.value += 1;
+  frontmatter.value = route.data?.frontmatter || {};
+  isFrontmatterLoaded.value = true;
+};
 
 // 监听路由变化，更新 key 和 frontmatter
 watch(() => route.path, () => {
-  componentKey.value += 1;
-  // 获取 Frontmatter
-  frontmatter.value = route.data?.frontmatter || {};
+  isFrontmatterLoaded.value = false;
+  updateKeyAndFrontmatter();
 });
+
+// 在组件挂载时更新 key 和 frontmatter
+onMounted(updateKeyAndFrontmatter);
 </script>
 
 <template>
@@ -23,7 +33,7 @@ watch(() => route.path, () => {
       意见反馈
     </h2>
     <AppSBox />
-    <!-- 仅在 Frontmatter 中未设置 hideChangelog 时渲染 GitChangelog -->
-    <NolebaseGitChangelog v-if="!frontmatter.hideChangelog" />
+    <!-- 仅在 Frontmatter 加载完成且未设置 hideChangelog 时渲染 GitChangelog -->
+    <NolebaseGitChangelog v-if="isFrontmatterLoaded && !frontmatter.hideChangelog" />
   </div>
 </template>
