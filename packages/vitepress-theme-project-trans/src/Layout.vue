eca59fa411604f2a0d726c707363832322f7e591
特菈 Dustella <fdnoaivj@outlook.com>
2024-09-19 13:58:15 +0800

    refactor: deprecate `MarkdownSectionWrapper`, use SFC layout.  (#424)

---
+++ packages/vitepress-theme-project-trans/src/Layout.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import DefaultTheme from 'vitepress/theme'
+import {
+  NolebaseHighlightTargetedHeading,
+} from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesScreenMenu,
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+
+import { useData } from 'vitepress'
+import CopyrightInfo from './components/CopyrightInfo.vue'
+import AppFooter from './components/AppFooter.vue'
+import AppearanceToggle from './components/AppearanceToggle.vue'
+import PageInfo from './components/PageInfo.vue'
+
+const { Layout } = DefaultTheme
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <AppearanceToggle>
+    <Layout>
+      <template #doc-before>
+        <div class="vp-doc">
+          <h1>{{ frontmatter.title }}</h1>
+          <PageInfo />
+          <CopyrightInfo />
+        </div>
+        <NolebaseHighlightTargetedHeading />
+      </template>
+      <template #doc-after>
+        <AppFooter />
+      </template>
+      <template #nav-bar-content-after>
+        <NolebaseEnhancedReadabilitiesMenu />
+      </template>
+      <template #nav-screen-content-after>
+        <NolebaseEnhancedReadabilitiesScreenMenu />
+      </template>
+    </Layout>
+  </AppearanceToggle>
+</template>
--- packages/vitepress-theme-project-trans/src/theme.ts
+++ packages/vitepress-theme-project-trans/src/theme.ts
@@ -5,48 +5,25 @@ import { h } from 'vue'
 import './custom-font.css'
 
 import {
-  NolebaseEnhancedReadabilitiesMenu,
   NolebaseEnhancedReadabilitiesPlugin,
-  NolebaseEnhancedReadabilitiesScreenMenu,
 } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
-
-import {
-  NolebaseHighlightTargetedHeading,
-} from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
 
 import {
   NolebaseGitChangelogPlugin,
 } from '@nolebase/vitepress-plugin-git-changelog/client'
 import type Options from 'vitepress-sidebar'
-import AppearanceToggle from './components/AppearanceToggle.vue'
 
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css'
 
-import AppFooter from './components/AppFooter.vue'
+import Layout from './Layout.vue'
 
 export type SidebarOptions = Options
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(AppearanceToggle, null, {
-      default: () => h(DefaultTheme.Layout, null, {
-        // https://vitepress.dev/guide/extending-default-theme#layout-slots
-        'doc-top': () => [
-          h(NolebaseHighlightTargetedHeading),
-        ],
-        'doc-after': () => [h(AppFooter)],
-        'nav-bar-content-after': () => [
-          h(NolebaseEnhancedReadabilitiesMenu),
-        ],
-        'nav-screen-content-after': () => [
-          h(NolebaseEnhancedReadabilitiesScreenMenu),
-        ],
-      }),
-    })
-  },
+  Layout,
   enhanceApp({ app }) {
     app.use(NolebaseEnhancedReadabilitiesPlugin, {
       spotlight: {
